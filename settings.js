3d8035c1d0e151d450fef4c45077a624ca81da07
Nyasha2005 <154772606+Nyasha2005@users.noreply.github.com>
2024-09-06 17:50:36 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -9,19 +9,19 @@ global.socialm = "GitHub: GlobalTechInfo" //ur github or insta name
 global.location = "Pakistan, Karachi, Karachi" //ur location
 
 //new
-global.botname = '© 𝐆𝐋𝐎𝐁𝐀𝐋-𝐌𝐃' //ur bot name
-global.ownernumber = ['923444844060'] //ur owner number, dont add more than one
-global.ownername = '© GlobalTechInfo' //ur owner name
+global.botname = '© LORD RAIDEN ' //ur bot name
+global.ownernumber = ['263718228938'] //ur owner number, dont add more than one
+global.ownername = '© NY.BMW.WT' //ur owner name
 global.websitex = "https://www.youtube.com/@GlobalTechInfo"
-global.wagc = "https://whatsapp.com/channel/0029VagJIAr3bbVBCpEkAM07"
-global.themeemoji = '🪀'
+global.wagc = "https://whatsapp.com/channel/0029VafmABz7Noa5NCDBfc0A"
+global.themeemoji = '⚡️'
 global.wm = "Xeon Bot Inc."
 global.botscript = 'https://github.com/GlobalTechInfo/GLOBAL-MD' //script link
 global.packname = "Sticker By"
 global.author = "GlobalTechInfo\n\n+923444844060"
-global.creator = "923444844060@s.whatsapp.net"
+global.creator = "263718228938@s.whatsapp.net"
 global.xprefix = '.'
-global.premium = ["923444844060"] // Premium User
+global.premium = ["263718228938"] // Premium User
 
 //channel id
 global.xchannel = {
